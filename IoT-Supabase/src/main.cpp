331c8f91fec7b849113dd9a670720f2c3ed051a9
Jhagas Winaya <jhagashana@mailbox.org>
2023-01-21 22:52:56 +0700

    penjelasan kode arduino

--- IoT-Supabase/src/main.cpp
+++ IoT-Supabase/src/main.cpp
@@ -25,12 +25,13 @@ const char *password = "wifi_password";
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter;
 
+// library untuk client HTTPS
 HTTPClient https;
 WiFiClientSecure client;
 
 unsigned long timeDelay = 300000; // berapa milidetik jeda tiap pengiriman data (5 menit)
 unsigned long lastTime = timeDelay;
-const int res = 5000; // refresh rate perangkat (5 detik)
+const int res = 60000; // refresh rate perangkat (1 menit)
 bool isSend = true;
 
 // tambahkan/kurangi sensor sesuai kebutuhan anda
@@ -44,6 +45,7 @@ String unit[jumlahSensor] = {"°C", "%", "Lux", ""};
 // nilai data sensor
 float value[jumlahSensor];
 
+// variabel untuk respon POST request HTTP
 int httpCode;
 
 void setup()
@@ -56,6 +58,7 @@ void setup()
   Wire.begin();
   lightMeter.begin();
 
+  // Koneksi ke Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -66,24 +69,34 @@ void setup()
 
 void loop()
 {
+  // t1 dan t2 untuk menghitung waktu yang ditempuh saat pengiriman data, agar konsisten jeda waktunya
   unsigned long t1 = millis();
 
   if (lastTime >= timeDelay)
   {
+    // -------------------------------
     // masukkan nilai sensor, jumlah masukan harus sesuai jumlah sensor (dalam contoh ini 4)
     value[0] = dht.readTemperature();
     value[1] = dht.readHumidity();
     value[2] = lightMeter.readLightLevel();
     value[3] = (-0.0693 * analogRead(A0)) + 7.3855; // Calibration formula from ADC to pH
+    // -------------------------------
 
+    // -------------------------------
+    // bila pembacaan sensor tidak terbaca, maka error dan data tidak dikirim. Gunakan logika OR
     if (isnan(value[0]) || isnan(value[1]) || value[1] == 1 || value[0] == -50)
     {
       Serial.println(F("Failed to read from DHT sensor!"));
       isSend = false;
       return;
     }
+    // -------------------------------
 
+    // Bahwa data sedang dikirim dan waktu tempuh kembali ke 0
     lastTime = 0;
+
+    // -------------------------------
+    // Buat struktur JSON
     String httpReqData = "";
     StaticJsonDocument<1024> doc;
     doc["refresh"] = timeDelay;
@@ -99,26 +112,24 @@ void loop()
     }
 
     serializeJson(doc, httpReqData);
+    // -------------------------------
 
+    // -------------------------------
+    // Kirim Data dengan HTTP POST
     if (https.begin(client, url + "/rest/v1/" + table))
     {
       https.addHeader("apikey", apikey);
       https.addHeader("Content-Type", "application/json");
-      https.addHeader("Prefer", "return=representation");
       httpCode = https.POST(httpReqData);
-      String payload = https.getString();
-      Serial.println(payload);
+      Serial.println(httpCode);
 
-      if ((payload.length() == 0))
-        isSend = false;
-      else
-        isSend = true;
+      isSend = true;
 
-      if (httpCode == 201 || httpCode == -11)
+      if (httpCode == 201 || httpCode == -11) // Bila pengiriman data berhasil
       {
         Serial.println("Success!!");
       }
-      else
+      else // Bila gagal
       {
         Serial.printf("[HTTPS] POST failed, error: %s\n", https.errorToString(httpCode).c_str());
         isSend = false;
@@ -126,18 +137,20 @@ void loop()
 
       https.end();
     }
-    else
+    else // Jika koneksi gagal
     {
       Serial.printf("[HTTPS] Unable to connect\n");
       isSend = false;
     }
   }
+  // -------------------------------
 
   unsigned long t2 = millis();
 
-  if (!isSend)
+  // logika berkaitan dengan pewaktu pengiriman data
+  if (!isSend) // bila gagal kirim, akan terus mencoba mengirim data lagi sampai bisa
     lastTime = timeDelay;
-  else
+  else // bila berhasil, perangkat akan delay sesuai dengan nilai refresh rate perangkat
   {
     int elapsed = res - (t2 - t1);
     lastTime += res;
