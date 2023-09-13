4f08cf6331d2853ea15649a021bc0346c7b24f67
Jhagas Hana Winaya <jhagashana@mailbox.org>
2023-09-13 10:08:27 +0700

    Update const.h

--- IoT-Supabase/src/const.h
+++ IoT-Supabase/src/const.h
@@ -1,25 +1,25 @@
 #include <Arduino.h>
 
 // 'supabase url' dan 'anon key' yang didapat dari laman supabase
-const String url = "supabase_url";
-const String apikey = "supabase_anon_key";
+const String url = "your_supabase_url";
+const String apikey = "your_supabase_API_key";
 const String table = "data";
 
 // masukkan nama wifi dan password yang ingin disambungkan
-const char *ssid = "wifi_ssid";
-const char *password = "wifi_password";
+const char *ssid = "your_ssid";
+const char *password = "your_ssid_password";
 
 unsigned long timeDelay = 300000; // berapa milidetik jeda tiap pengiriman data (5 menit)
 bool isSend = true;
 
 // tambahkan/kurangi sensor sesuai kebutuhan anda
-const int jumlahSensor = 4;
+const int jumlahSensor = 5;
 // nama sensor untuk ditampilkan pada web, isi pada array harus sesuai jumlah sensor
-String name[jumlahSensor] = {"Suhu", "Kelembapan Relatif", "Iluminansi", "pH Tanah"};
+String name[jumlahSensor] = {"Suhu", "Kelembapan Udara", "Iluminansi", "Kelembaban Tanah", "Pompa"};
 // tipe sensor untuk ikon web, isi pada array harus sesuai jumlah sensor
-String type[jumlahSensor] = {"temperature", "relativeHumidity", "illuminance", "ph"};
+String type[jumlahSensor] = {"temperature", "relativeHumidity", "illuminance", "soilMoisture" "On/Off"};
 // satuan data sensor untuk tampilan web, isi pada array harus sesuai jumlah sensor
-String unit[jumlahSensor] = {"°C", "%", "Lux", ""};
+String unit[jumlahSensor] = {"Â°C", "%", "Lux", "%", ""};
 // nilai data sensor
 float value[jumlahSensor];
 
@@ -36,9 +36,19 @@ String makeJSON(float value[])
         sensor["name"] = name[i];
         sensor["type"] = type[i];
         sensor["unit"] = unit[i];
-        sensor["value"] = value[i];
+        if (i==4){
+            if (value[i]==0){
+                sensor["value"] = "Mati";
+            }
+            else {
+                sensor["value"] = "Nyala";
+            }
+        } 
+        else {
+            sensor["value"] = value[i];
+        }
     }
     serializeJson(doc, httpReqData);
-
+    
     return httpReqData;
 }
